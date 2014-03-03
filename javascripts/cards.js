ac317aaa5d1f74e739b8904cbdcd4c27de492df2
Phoebe Lin <phoebe.lin93@gmail.com>
2014-03-03 12:24:41 -0800

    moved js file

--- javascripts/cards.js
+++ javascripts/cards.js
@@ -1,10 +1,10 @@
 /*Creating arrays with each specific type of card*/
 
-var hearts = new Array();
+var hearts = new Array("images/1.png");
 
 
 
 function display_cards(int probability, int num_cards) {
-
+    
 
 }
