392dcabfc752c60adf25df1df319ef5eb6458138
CognitiveGrowth <Falk@airbears2-10-142-158-84.airbears2.1918.berkeley.edu>
2014-11-10 20:49:51 -0800

    Commented out console.log statements, because they caused trouble.

--- javascripts/cards.js
+++ javascripts/cards.js
@@ -52,10 +52,10 @@ function display_cards(total_cards, feature, i) {
 	shuffleArray(spades);
 	//i = getRandomInt(1,8);
 	var num_feature_cards = new_iterate_array[q];
-	console.log(num_feature_cards);
+	//console.log(num_feature_cards);
 	win_probability = num_feature_cards/total_cards;
-	console.log("win probability");
-	console.log(win_probability);
+	//console.log("win probability");
+	//console.log(win_probability);
 	//var num_feature_cards = 5
 	var num_remaining_cards = total_cards - num_feature_cards;
 	
@@ -86,12 +86,12 @@ function display_cards(total_cards, feature, i) {
 	    card.src = master_array[i];
 	    card.setAttribute("id", "cards"+i);
 	    if(document.getElementById("card-grid").title != "") {
-		console.log("node getting replaced");
+		//console.log("node getting replaced");
 		//document.getElementById("card-grid").innerHTML = 
 		document.getElementById("card-grid").replaceChild(card, document.getElementById("cards"+i));
 		//document.getElementById("card-grid").appendChild(card);
 	    } else {
-		console.log("node getting appended");
+		//console.log("node getting appended");
 		document.getElementById("card-grid").appendChild(card);
 	    }
 	}
@@ -124,23 +124,23 @@ y = data['probabilities'];
 data['payoffs'] = [];
 z = data['payoffs'];
 // var amount = document.getElementById("amount-box").value;
-console.log(document.getElementById("amount-box").value);
+//console.log(document.getElementById("amount-box").value);
     function nextTrial() {
 	//document.getElementById("amount-box").reset();
 	if (document.getElementById("amount-box").value == "") {
 	    window.alert("You must input a number");
 	} else {
-	    console.log("amount: " + document.getElementById("amount-box").value);
+	    //console.log("amount: " + document.getElementById("amount-box").value);
 	    //var datacell = {probability: win_probability};
-	    console.log(win_probability);
+	    //console.log(win_probability);
 	    y.push(win_probability);
 	    x.push(document.getElementById("amount-box").value);
 	    z.push([feature_payoff[payoff_array[j]], other_payoff[payoff_array[j]]]);
-	    console.log("x" , x);
+	    //console.log("x" , x);
 	    data['prices'] = x;
 		data['probabilities'] = y;
 		data['payoffs'] = z;
-	    console.log(data);
+	    //console.log(data);
 
 	    display_cards(total_cards, feature, new_iterate_array[q]);
 	    q++;
@@ -151,13 +151,13 @@ console.log(document.getElementById("amount-box").value);
 	}
 	if(current_trial_num > num_trials-1) {
 		
-		console.log('data before turk', data);
+		//console.log('data before turk', data);
 		submitTurk();
-	    console.log("data submitted");
+	    //console.log("data submitted");
 	    document.getElementById("next-button").disabled = true;
 	}
 	
-	console.log("current trial number: " + current_trial_num);
+	//console.log("current trial number: " + current_trial_num);
 	
     }
 
