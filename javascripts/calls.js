a708bf2aacb3dba8c2da94c6dd9003f345883173
CognitiveGrowth <Falk@airbears2-10-142-159-138.airbears2.1918.berkeley.edu>
2014-11-17 19:05:46 -0800

    commented out console.log

--- javascripts/calls.js
+++ javascripts/calls.js
@@ -2,7 +2,7 @@
 window.onload = function() {
     var amount = document.getElementById("amount-box").value;
     var datacell = {};
-    console.log("data: " + data);
+    //console.log("data: " + data);
     printHeader(feature_payoff, other_payoff);
     display_cards(total_cards, feature);
     nextTrial();
